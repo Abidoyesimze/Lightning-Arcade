84a9bf31b995ca781be029ace3196a74663810ee
Similoluwa Abidoye <similoluwaeyitayoabidoye@gmail.com>
2025-07-29 13:41:07 +0100

    updates: configure the abis and the file for calling all the contract abis

---
+++ frontend/src/abi/contracts.ts
@@ -0,0 +1,30 @@
+
+import GameManagerABI from './GameManager.json';
+import PlayerProfileABI from './PlayerProfile.json';
+import RewardsABI from './Rewards.json';
+import TournamentABI from './Tournament.json';
+
+
+
+// Export ABIs
+export const GAmeManagerContract = {
+    address: '0x4A4EBc7bfb813069e5495fB36B53cc937A31b441',
+    abi: GameManagerABI
+}
+
+export const PlayerProfileContract = {
+    address: '0x9f853686c5162A8E210dc9D13a8114f095Fc17F3',
+    abi: PlayerProfileABI
+}
+
+export const LightningArcadeRewardsContract = {
+    address: '0xbF1B2ca2CC17Bd98679D584575d549c62B3214eb',
+    abi: RewardsABI
+}
+
+export const TournamentContract = {
+    address: '0x88deaBcDDdAD618aBd67eCBdf490f68646c088aD',
+    abi: TournamentABI
+}
+
+
