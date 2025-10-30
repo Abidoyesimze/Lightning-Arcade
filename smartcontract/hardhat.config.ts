9c78d0a31af9ef8baff899c5df05176dda23796f
Samuel1505 <oluwakoredeakinola@gmail.com>
2025-10-30 18:41:09 +0100

    set up hardhat configuration file

--- smartcontract/hardhat.config.ts
+++ smartcontract/hardhat.config.ts
@@ -1,26 +1,22 @@
 import type { HardhatUserConfig } from "hardhat/config";
-
 import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
 import { configVariable } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   plugins: [hardhatToolboxMochaEthersPlugin],
+  
   solidity: {
-    profiles: {
-      default: {
-        version: "0.8.28",
-      },
-      production: {
-        version: "0.8.28",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
+  version: "0.8.28",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
     },
+    viaIR: true, // ✅ works in v3 when applied directly here
   },
+},
+
+
   networks: {
     hardhatMainnet: {
       type: "edr-simulated",
@@ -35,6 +31,21 @@ const config: HardhatUserConfig = {
       chainType: "l1",
       url: configVariable("SEPOLIA_RPC_URL"),
       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    },
+    baseSepolia: {
+      url: "https://sepolia.base.org",
+      accounts: [configVariable("BASE_SEPOLIA_PRIVATE_KEY")],
+      chainId: 84532,
+      type: "http",
+      chainType: "l1",
+    },
+  },
+
+  // ✅ Global verify config (new Hardhat v3 structure)
+  verify: {
+    etherscan: {
+      apiKey: configVariable("ETHERSCAN_API_KEY"),
+      //apiUrl: "https://api-sepolia.basescan.org",
     },
   },
 };
